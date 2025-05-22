beec2c5f4e4cb1c4482e8a2695dc2f7db7b5007b
Lucas <us3.lucas@gmail.com>
2025-05-22 15:29:49 -0300

    Update Customer.java

--- src/entities/Customer.java
+++ src/entities/Customer.java
@@ -1,43 +1,68 @@
 package entities;
 
-
+/**
+ * Classe que representa um cliente da loja de eletrônicos.
+ * Armazena informações básicas de contato do cliente.
+ */
 public class Customer {
-    private String name;
-    private String email;
-    private String phone;
+
+    // Atributos privados (encapsulamento)
+    private String name;   // Nome do cliente
+    private String email;  // Email para contato
+    private String phone;  // Telefone para contato
 
     // Construtores, getters e setters
-        public Customer () {
+
+    /**
+     * Construtor padrão
+     */
+    public Customer() {
     }
 
-        public Customer (String name, String email, String phone) {
-                this.name = name;
-                this.email = email;
-                this.phone = phone;
-        }
-        public String getName() {
-      return name;
+    /**
+     * Construtor com parâmetros
+     * @param name Nome do cliente
+     * @param email Email do cliente
+     * @param phone Telefone do cliente
+     */
+    public Customer(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
     }
-        public void setName(String name) {
-      this.name = name;
-    }
-        public String getEmail() {
-      return email;
-    }
-        public void setEmail(String email) {
-      this.email = email;
-    }
-        public String getPhone() {
-      return phone;
-    }
-        public void setPhone(String phone) {
-                this.phone = phone;
-        }
 
-        // Método para exibir informações completas do cliente
+    // Métodos getters e setters
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Sobrescreve o método toString para exibir os dados do cliente formatados
+     * @return String formatada com os dados do cliente
+     */
     @Override
     public String toString() {
-        return name + " (" + email + ")" + " - Telefone: " + phone;
+        return name + " (" + email + ")";
     }
 }
