4988ccc8f90fc3102d3baf8e54893faf99b995c7
Lucas <us3.lucas@gmail.com>
2025-05-22 15:30:11 -0300

    Update ProductCategory.java

--- src/entities/enums/ProductCategory.java
+++ src/entities/enums/ProductCategory.java
@@ -1,10 +1,15 @@
 package entities.enums;
 
+/**
+ * Enumeração que define as categorias de produtos eletrônicos disponíveis na loja.
+ * Utilizar enum garante tipo seguro e evita erros de digitação ou valores inválidos.
+ */
 public enum ProductCategory {
-    SMARTPHONE,
-    LAPTOP,
-    TV,
-    ACCESSORY,
-    GAMING,
-    OTHER
+
+    SMARTPHONE,  // Telefones celulares inteligentes
+    LAPTOP,      // Notebooks e laptops
+    TV,          // Televisores
+    ACCESSORY,   // Acessórios como fones, cabos, carregadores
+    GAMING,      // Produtos para jogos (consoles, controles)
+    OTHER        // Outros produtos que não se encaixam nas categorias acima
 }
